5bcbb9afdcbd5fb5c36f93756833720f640d0203
Milan Fillipi <milanfillipi@seattleacademy.org>
2018-10-22 12:34:04 -0700

    update course name and reqs

--- js/reqs.js
+++ js/reqs.js
@@ -1,26 +1,29 @@
-var reqs = [];
+  var reqs = [];
 
-function makeReq(name, count) {
-    var req = {};
-    req.name = name;
-    req.count = count;
-    return req;
-}
+  function makeReq(name, count) {
+      var req = {};
+      req.name = name;
+      req.count = count;
+      return req;
+  }
 
-//Derived from http://math.seattleacademy.org/milanfillipi/testforday17.html
-reqs.push(makeReq("English", 12));
-reqs.push(makeReq("History", 9));
-reqs.push(makeReq("Science", 9));
-reqs.push(makeReq("Math", 9));
-reqs.push(makeReq("World Languages", 9));
-reqs.push(makeReq("CompThinking", 2));
-reqs.push(makeReq("Innovations", 2));
-reqs.push(makeReq("Entrepreneurship/Financial Literacy", 2));
-reqs.push(makeReq("Health", 2));
-reqs.push(makeReq("P.E.", 4));
-reqs.push(makeReq("Dance", 1));
-reqs.push(makeReq("Music", 1));
-reqs.push(makeReq("Visual", 1));
-reqs.push(makeReq("Theater", 1));
-reqs.push(makeReq("Assorted Arts", 3));
-reqs.push(makeReq("Additional Credits", 13));
+
+
+  reqs.push(makeReq("English", 12));
+  reqs.push(makeReq("History", 9));
+  reqs.push(makeReq("Science", 9));
+  reqs.push(makeReq("Math", 9));
+  reqs.push(makeReq("Foreign Languague", 9));
+  reqs.push(makeReq("Math/Science", 3));
+  reqs.push(makeReq("CompThinking/AI", 2));
+  reqs.push(makeReq("Innovations", 2));
+  reqs.push(makeReq("Entrepreneurship/FL", 2));
+  reqs.push(makeReq("Health", 2));
+  reqs.push(makeReq("P.E.", 4));
+  reqs.push(makeReq("Dance", 1));
+  reqs.push(makeReq("Music", 1));
+  reqs.push(makeReq("Visual", 1));
+  reqs.push(makeReq("Theater", 1));
+  reqs.push(makeReq("Assorted Arts", 3));
+  reqs.push(makeReq("Additional Credits", 13));
+  reqs.push(makeReq("Total Credits", 84));
